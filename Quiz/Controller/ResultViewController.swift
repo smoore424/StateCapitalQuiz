2e2f1db643c384385e09530cc2584b4bb429156d
smoore424 <smoore424@gmail.com>
2021-09-03 05:48:02 -0700

    Clean up

--- Quiz/Controller/ResultViewController.swift
+++ Quiz/Controller/ResultViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ResultViewController: UIViewController {
 
+    let layer = CAEmitterLayer()
     let defaults = UserDefaults.standard
     var score = Int()
     
@@ -36,8 +37,10 @@ class ResultViewController: UIViewController {
             messageLabel.text = "You're getting there!"
         case 41...49:
             messageLabel.text = "Well Done!"
+            confetti()
         case 50:
             messageLabel.text = "Congratulations! Perfect Score!"
+            confetti()
         default:
             print("error")
         }
@@ -49,6 +52,44 @@ class ResultViewController: UIViewController {
         defaults.set(score, forKey: K.score)
     }
 
+    //MARK: - Confetti Animation Effect
+    func confetti() {
+        createConfetti()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.8) {
+            self.stopConfetti()
+        }
+    }
+    
+    func createConfetti() {
+        layer.emitterPosition = CGPoint(x: view.center.x, y: -150)
+        //layer.preservesDepth = true
+        
+        let colors = [UIColor.systemRed, UIColor.systemBlue, UIColor.systemGreen, UIColor.systemPink, UIColor.systemTeal, UIColor.systemOrange, UIColor.systemYellow, UIColor.systemPurple, UIColor.systemIndigo]
+        
+        let cells: [CAEmitterCell] = colors.compactMap {
+            let cell = CAEmitterCell()
+            cell.scaleRange = 0.2
+            cell.scale = 0.05 - cell.scaleRange
+            cell.birthRate = 100
+            cell.emissionRange = .pi * 2
+            cell.spin = 2
+            cell.setValue("plane", forKey: "particleType")
+            cell.setValue(Double.pi, forKey: "orientationRange")
+            cell.setValue(Double.pi / 2, forKey: "orientationLongitude")
+            cell.setValue(Double.pi / 2, forKey: "orientationLatitude")
+            cell.lifetime = 10
+            cell.velocity = 200
+            cell.color = $0.cgColor
+            cell.contents = UIImage(named: "whiteSquare")!.cgImage
+            return cell
+        }
+        layer.emitterCells = cells
+        view.layer.addSublayer(layer)
+    }
+    
+    func stopConfetti() {
+        layer.lifetime = 0
+    }
 }
 
 
